0a26163ef098ae91e3c4c31b7021f44dc2bbbb45
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2022-11-30 20:41:34 -0600

    Added new project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -20,8 +20,9 @@ import GitIcon from '../../assets/svg/languages/GitIcon';
 // Project Images
 import projectImage1 from '../../assets/img/payroll-sorter-image.webp';
 import projectImage2 from '../../assets/img/pdf-word-converter-image.webp';
-import projectImage3 from '../../assets/img/mgh-menu-image.webp';
-import projectImage4 from '../../assets/img/my-coding-tracker-image.webp';
+import projectImage3 from '../../assets/img/mh-weakener-bot-image.webp';
+import projectImage4 from '../../assets/img/mgh-menu-image.webp';
+import projectImage5 from '../../assets/img/my-coding-tracker-image.webp';
 
 const Projects = props => {
   const projectGridItem = PROJECTS_DATA.map(project => (
@@ -58,7 +59,7 @@ const PROJECTS_DATA = [
   {
     id: 0,
     image: projectImage1,
-    altText: 'alt text here',
+    altText: 'Payroll Report Sorter App',
     title: 'Payroll Report Sorter',
     description:
       "A personal program to sort staff's time-clocks in payroll reports between multiple facilities.",
@@ -69,7 +70,7 @@ const PROJECTS_DATA = [
   {
     id: 1,
     image: projectImage2,
-    altText: 'alt text here',
+    altText: 'PDF to Word Converter App',
     title: 'PDF/Word Converter',
     description:
       'Simple program that converts pdf <-> docx while maintaining proper formatting.  Used to help with my work involving editing .pdf files from time-to-time.',
@@ -80,7 +81,18 @@ const PROJECTS_DATA = [
   {
     id: 2,
     image: projectImage3,
-    altText: 'alt text here',
+    altText: 'Monster Hunter Discord Bot',
+    title: 'MH Weakener Bot',
+    description:
+      "A discord bot that shows a brief overview of a monster's location, resistances, and weaknesses and any special conditions, if any.  Allows user and friends to be on the same page and to quickly optimize for a fight.",
+    tags: [JavaScriptIcon, NodeJSIcon],
+    gitHubPath: 'https://github.com/AndrewKohn/mh-weakener-discord-bot',
+    liveSitePath: 'https://discord.com/invite/S5kHXjZfjJ',
+  },
+  {
+    id: 3,
+    image: projectImage4,
+    altText: 'Morning Glory Homes menu site',
     title: 'MGH Menu',
     description: 'This project allows the user to view/submit weekly menus.',
     tags: [HtmlIcon, CssIcon, JavaScriptIcon],
@@ -88,9 +100,9 @@ const PROJECTS_DATA = [
     liveSitePath: 'https://mghmenu.netlify.app/',
   },
   {
-    id: 3,
-    image: projectImage4,
-    altText: 'alt text here',
+    id: 4,
+    image: projectImage5,
+    altText: 'My Coding Tracker App',
     title: 'My Coding Tracker',
     description:
       "Simple timer to count the amount of time spent actively coding. This timer should not be spent on solely browsing videos, podcasts, books, & etc even if they're related.",
@@ -99,21 +111,10 @@ const PROJECTS_DATA = [
     liveSitePath: '',
   },
   {
-    id: 4,
-    image: exImg,
-    altText: 'alt text here',
-    title: 'Project 5',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?',
-    tags: [],
-    gitHubPath: '',
-    liveSitePath: '',
-  },
-  {
     id: 5,
     image: exImg,
     altText: 'alt text here',
-    title: 'Project 6',
+    title: 'Project 5',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?',
     tags: [],
