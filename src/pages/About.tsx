a07d81feae86192702cde7f6f5872e5b92f22a7d
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-04-02 02:45:44 -0500

    Add about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,16 +1,52 @@
+import { useEffect, useState } from 'react';
+import Avatar from '../assets/svg/avatars/Avatar';
+import AvatarClosed from '../assets/svg/avatars/AvatarClosed';
+import Card from '../components/UI/Card';
 import './Style.scss';
 
-interface Props {}
-
 const About = ({}) => {
+  const [isEyesClosed, setIsEyesClosed] = useState<boolean>(false);
+
+  useEffect(() => {
+    const blinkTiming = Math.floor(Math.random() * 700);
+
+    isEyesClosed
+      ? setTimeout(() => {
+          // Avatar eyes are open
+          setIsEyesClosed(!isEyesClosed);
+        }, 3500)
+      : setTimeout(() => {
+          // Avatar blinks
+          setIsEyesClosed(!isEyesClosed);
+        }, blinkTiming);
+  }, [isEyesClosed]);
+
   return (
     <div className="container">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat
-        expedita minima maxime eum deserunt voluptatum nesciunt, reprehenderit
-        doloremque, exercitationem sunt odio, sint explicabo dolorum error
-        velit. Facilis aliquam quidem aperiam.
-      </p>
+      <h2>ABOUT</h2>
+      <div className="about-wrapper">
+        <div className="avatar-container">
+          {isEyesClosed ? (
+            <Avatar className="avatar" />
+          ) : (
+            <AvatarClosed className="eyes-closed" />
+          )}
+        </div>
+        <Card>
+          <p className="about-text">
+            A <strong>Web & Software Developer</strong> leveraging a background
+            in healthcare that taught me discipline, achieving deadlines, and
+            pursuing continuous growth making me a strong addition to any
+            development team.
+          </p>
+          <p className="about-text">
+            I am a highly-motivated and committed to pursuing a career change in{' '}
+            <strong>Full-Stack Development</strong>. I primarily use JavaScript
+            & its frameworks for the Front-End and Java for the Back-End, but
+            picking up a new language or framework is always exciting!
+          </p>
+        </Card>
+      </div>
     </div>
   );
 };
