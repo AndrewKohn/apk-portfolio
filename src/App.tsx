3a6e496088071e39b8fea6d6b72baef5792730ec
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-04-03 04:02:40 -0500

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
 
 const App = ({}) => {
   return (
@@ -16,6 +17,7 @@ const App = ({}) => {
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </main>
   );
 };
