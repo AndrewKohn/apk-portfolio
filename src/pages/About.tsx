2db004767c83195bd230db856f6933f0042e7b59
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-04-09 17:03:11 -0500

    Add skills component and more text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,6 +8,7 @@ import resumeFile from '../assets/apk-resume.pdf';
 import { Link } from 'react-router-dom';
 import Background from '../components/UI/Background';
 import { BsBoxArrowUpRight } from 'react-icons/bs';
+import Skills from '../components/Skills/Skills';
 
 const About = ({}) => {
   const [isEyesClosed, setIsEyesClosed] = useState<boolean>(false);
@@ -51,11 +52,10 @@ const About = ({}) => {
                 addition to any development team.
               </p>
               <p className="about-text">
-                I am a highly-motivated and committed to pursuing a career
-                change in <strong>Full-Stack Development</strong>. I primarily
-                use JavaScript & its frameworks for the Front-End and Java for
-                the Back-End, but picking up a new language or framework is
-                always exciting!
+                I love the process of taking an idea and bringing it to life
+                through code. I'm constantly working to improve my skills and
+                stay up-to-date with the latest developments in software
+                development.
               </p>
             </Card>
           </div>
@@ -70,6 +70,57 @@ const About = ({}) => {
               <Button>CONTACT ME</Button>
             </Link>
           </div>
+        </div>
+
+        <div className="about-skills-container">
+          <div className="text-skills-wrapper">
+            <Card classes="text-container">
+              <p className="about-text">
+                While I am pursuing a degree in Computer Science, at my core I
+                am a self-taught developer that started their journey with{' '}
+                <strong>C</strong> & <strong>Java</strong>. As a self-taught
+                developer, I understand the importance of continuous learning
+                and improvement. I'm always seeking out new resources and
+                tutorials to expand my skills, and I enjoy collaborating with
+                other developers to share knowledge and learn from their
+                experiences.
+              </p>
+            </Card>
+            <Card classes="text-container">
+              <p className="about-text">
+                One of my main focuses right now is{' '}
+                <strong>contributing to open-source projects</strong>. I believe
+                that collaborating with other developers and contributing to the
+                community is not only a great way to learn and improve, but also
+                a way to give back to the community. I'm currently volunteering
+                my spare time and skills to help others and the environment.
+              </p>
+            </Card>
+          </div>
+
+          <Skills className="about-skills" />
+        </div>
+
+        <div className="about-wrapper">
+          <Card classes="text-container">
+            <p className="about-text">
+              What I love most about being a developer is the opportunity to{' '}
+              <strong>solve problems</strong> and <strong>create tools</strong>{' '}
+              that make people's lives easier. I believe that good design and
+              user experience are <strong>essential</strong> to building
+              software that's not only functional but enjoyable to use.
+            </p>
+          </Card>
+          <Card classes="text-container">
+            <p className="about-text">
+              When I'm not coding, I enjoy hiking & camping, woodworking,
+              gardening, and exploring new recipes in the kitchen.
+            </p>
+            <p className="about-text align-left">
+              If you read this far, <strong>thank you</strong> for visiting my
+              site!
+            </p>
+          </Card>
         </div>
       </div>
     </div>
