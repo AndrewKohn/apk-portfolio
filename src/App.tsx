1296d1593e35379eee25ca5b4342e617425c495a
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-04-09 17:00:41 -0500

    Add lazy load pages & loading bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import './App.scss';
+import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import PROJECTS_DATA from './data/ProjectsData';
 import ProjectPage from './pages/ProjectPage';
+import PageNotFound from './pages/PageNotFound';
+import LoadingBars from './components/UI/LoadingBars';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Projects from './pages/Projects';
+// import Contact from './pages/Contact';
+
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+const Contact = lazy(() => import('./pages/Contact'));
+const Projects = lazy(() => import('./pages/Projects'));
 
 const App = ({}) => {
   const projectsRoutes = PROJECTS_DATA.map((project, index: number) => {
@@ -23,15 +31,18 @@ const App = ({}) => {
   return (
     <main>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
+      <Suspense fallback={<LoadingBars />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<PageNotFound />} />
 
-        {/* Project Pages */}
-        {projectsRoutes}
-      </Routes>
+          {/* Project Pages */}
+          {projectsRoutes}
+        </Routes>
+      </Suspense>
       <Footer />
     </main>
   );
