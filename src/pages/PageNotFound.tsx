e8f9880891ddfd2a1af660244368d9ff71cb8260
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-04-09 17:07:00 -0500

    Add new LoadingBars & 404 page component

---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,16 @@
+import Background from '../components/UI/Background';
+import './style.scss';
+
+const PageNotFound = ({}) => {
+  return (
+    <div className="container page-not-found-container">
+      <Background className="page-not-found-bg" />
+      <h2 className="page-not-found-heading">404</h2>
+      <div className="content">
+        <p className="page-not-found-text">PAGE NOT FOUND</p>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
