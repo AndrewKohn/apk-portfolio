2184d73e0441fd5af6ee891ee2831b75ee2b7aae
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-06-03 16:56:33 -0500

    Add mui skeleton component

---
+++ src/components/ProjectGridItem/ProjectGridItemSkeleton.tsx
@@ -0,0 +1,69 @@
+import './ProjectGridItemSkeleton.scss';
+import Skeleton from '@mui/material/Skeleton';
+import Card from '../UI/Card';
+
+const ProjectGridItemSkeleton = ({}) => {
+  return (
+    <Card classes="skeletonCard">
+      <Skeleton
+        variant="rectangular"
+        width="300px"
+        height="200px"
+        animation="wave"
+        sx={{ backgroundColor: 'rgba(227, 227, 227, 0.1)' }}
+      />
+      <div className="middle-row">
+        <Skeleton
+          variant="text"
+          width="225px"
+          sx={{
+            fontSize: '3.2rem',
+            backgroundColor: 'rgba(227, 227, 227, 0.1)',
+          }}
+          animation="wave"
+        />
+        <Skeleton
+          variant="rectangular"
+          width="300px"
+          height="140px"
+          animation="wave"
+          sx={{ backgroundColor: 'rgba(227, 227, 227, 0.1)' }}
+        />
+        <div className="bottom-row">
+          <div className="links-wrapper">
+            <Skeleton
+              variant="rounded"
+              width="50px"
+              height="54px"
+              animation="wave"
+              sx={{ backgroundColor: 'rgba(227, 227, 227, 0.1)' }}
+            />
+            <Skeleton
+              variant="rounded"
+              width="50px"
+              height="54px"
+              animation="wave"
+              sx={{ backgroundColor: 'rgba(227, 227, 227, 0.1)' }}
+            />
+            <Skeleton
+              variant="rounded"
+              width="50px"
+              height="54px"
+              animation="wave"
+              sx={{ backgroundColor: 'rgba(227, 227, 227, 0.1)' }}
+            />
+          </div>
+          <Skeleton
+            variant="rounded"
+            width="90px"
+            height="50px"
+            animation="wave"
+            sx={{ backgroundColor: 'rgba(227, 227, 227, 0.1)' }}
+          />
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default ProjectGridItemSkeleton;
