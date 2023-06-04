6c563af968e25115eaf71af95e33d8f0dae655bc
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-06-04 08:27:56 -0500

    Remove skeleton from heading

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -16,46 +16,37 @@ const Heading = ({}) => {
 
   return (
     <Card classes="intro-card">
-      {!loading ? (
-        <div className="intro-wrapper">
-          <div className="heading-wrapper">
-            <h1>
-              Andrew
-              <br />
-              <span>Kohn</span>
-            </h1>
-            <SocialsList iconType="sm" />
-          </div>
-
-          <ul className="descriptions">
-            <li className="description">
-              <CheckMark className="checkmark" />
-              <p>
-                Front-end <strong>JavaScript/React Developer</strong>
-              </p>
-            </li>
-            <li className="description">
-              <CheckMark className="checkmark" />
-              <p>
-                Back-end <strong>Nodejs & Java Developer</strong>
-              </p>
-            </li>
-            <li className="description">
-              <CheckMark className="checkmark" />
-              <p>
-                A <strong>Detailed-oriented Self-Starter</strong>
-              </p>
-            </li>
-          </ul>
+      <div className="intro-wrapper">
+        <div className="heading-wrapper">
+          <h1>
+            Andrew
+            <br />
+            <span>Kohn</span>
+          </h1>
+          <SocialsList iconType="sm" />
         </div>
-      ) : (
-        <Skeleton
-          variant="rounded"
-          width="895px"
-          height="312px"
-          sx={{ backgroundColor: 'rgba(50, 50, 50, 0.05)' }}
-        />
-      )}
+
+        <ul className="descriptions">
+          <li className="description">
+            <CheckMark className="checkmark" />
+            <p>
+              Front-end <strong>JavaScript/React Developer</strong>
+            </p>
+          </li>
+          <li className="description">
+            <CheckMark className="checkmark" />
+            <p>
+              Back-end <strong>Nodejs & Java Developer</strong>
+            </p>
+          </li>
+          <li className="description">
+            <CheckMark className="checkmark" />
+            <p>
+              A <strong>Detailed-oriented Self-Starter</strong>
+            </p>
+          </li>
+        </ul>
+      </div>
     </Card>
   );
 };
