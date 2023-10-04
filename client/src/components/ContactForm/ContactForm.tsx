eea9d5dca3a1fd6820d4f67d8f2ccc357f5eed06
andrewkohn <contact@kohnandrew.com>
2023-10-04 13:28:13 -0500

    Update: changed fetch URL

--- client/src/components/ContactForm/ContactForm.tsx
+++ client/src/components/ContactForm/ContactForm.tsx
@@ -132,13 +132,16 @@ const ContactForm = ({
 
     // TODO: AWS for backend deployment
     try {
-      const response = await fetch('http://localhost:5000/post', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ userInputs }),
-      });
+      const response = await fetch(
+        'https://nameless-scrubland-27606-f38e606cfa40.herokuapp.com/post',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ userInputs }),
+        }
+      );
 
       if (response.ok) {
         setSubmission({ isSent: true, isLoading: false });
